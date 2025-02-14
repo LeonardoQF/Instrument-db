ebfac40ed2eadb32636897a4f7a14cf2f875eebc
LeonardoQF <leonardofrazao2015@outlook.com>
2025-02-14 11:02:07 -0300

    Changed controller responses to be ResponseEntity

--- src/main/java/com/uajj/Tests/controller/InstrumentController.java
+++ src/main/java/com/uajj/Tests/controller/InstrumentController.java
@@ -1,8 +1,10 @@
 package com.uajj.Tests.controller;
 
+import java.net.URI;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.uajj.Tests.model.entities.Instrument;
 import com.uajj.Tests.model.entities.enums.InstrumentType;
@@ -26,29 +29,33 @@ public class InstrumentController {
 	}
 	
 	@GetMapping
-	public List<Instrument> getAllInstruments() {
-		return service.findAll();
+	public ResponseEntity<List<Instrument>> getAllInstruments() {
+		return ResponseEntity.ok(service.findAll());
 	}
 	
 	@GetMapping(path = "/{id}")
-	public Instrument getInstrumentById(@PathVariable String id) {
-		
-		return service.findById(UUID.fromString(id));
+	public ResponseEntity<Instrument> getInstrumentById(@PathVariable(name = "id") String id) {
+		Instrument foundInstrument = service.findById(UUID.fromString(id));
+		return ResponseEntity.ok(foundInstrument);
 	}
 	
 	@PostMapping
-	public Instrument addInstrument(@RequestBody(required = true) Instrument instrument) {
-		return service.save(instrument);
+	public ResponseEntity<Instrument> addInstrument(@RequestBody(required = true) Instrument instrument) {
+		service.save(instrument);
+		
+		URI uri = (ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(instrument.getId().toString()).toUri());
+		
+		return ResponseEntity.created(uri).body(instrument);
 	}
 	
 	@GetMapping("/filter")
-	public List<Instrument> getAllInstrumentsByType(@RequestParam(required = false) String type) {
+	public List<Instrument> getAllInstrumentsByType(@RequestParam(required = true, name = "type") String type) {
 		String upperCaseType = type.toUpperCase();
 		return service.findAllByType(InstrumentType.valueOf(upperCaseType));
 	}
 	
 	@GetMapping("/search")
-	public List<Instrument> search(@RequestParam(required = true) String text) {
+	public List<Instrument> search(@RequestParam(required = true, name = "text") String text) {
 		return service.searchByAnyText(text);
 	}
 	
--- src/main/java/com/uajj/Tests/repository/InstrumentRepository.java
+++ src/main/java/com/uajj/Tests/repository/InstrumentRepository.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.uajj.Tests.model.entities.Instrument;
 import com.uajj.Tests.model.entities.enums.InstrumentType;
@@ -16,6 +17,6 @@ public interface InstrumentRepository extends JpaRepository<Instrument, UUID> {
 	@Query("SELECT i FROM Instrument i WHERE LOWER(i.brand) LIKE LOWER(CONCAT('%', :text, '%')) "
 			+ "OR LOWER(i.type) LIKE LOWER(CONCAT('%', :text, '%')) "
 			+ "OR LOWER(i.name) LIKE LOWER(CONCAT('%', :text, '%'))")
-	public List<Instrument> searchByAnyText(String text);
+	public List<Instrument> searchByAnyText(@Param(value = "text") String text);
 
 }
