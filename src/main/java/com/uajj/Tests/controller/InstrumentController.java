d3661b0ed269300b44f8d1ae82aca7ae23307eba
LeonardoQF <leonardofrazao2015@outlook.com>
2025-02-21 10:58:37 -0300

    Development Branch created, first commit and TODO

--- src/main/java/com/uajj/Tests/controller/InstrumentController.java
+++ src/main/java/com/uajj/Tests/controller/InstrumentController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.uajj.Tests.model.entities.Instrument;
@@ -40,7 +41,9 @@ public class InstrumentController {
 	}
 	
 	@PostMapping
-	public ResponseEntity<Instrument> addInstrument(@RequestBody(required = true) Instrument instrument) {
+	public ResponseEntity<Instrument> addInstrument(@RequestBody(required = true) Instrument instrument, MultipartFile images) {
+		//TODO Image upload logic
+		
 		service.save(instrument);
 		
 		URI uri = (ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(instrument.getId().toString()).toUri());
